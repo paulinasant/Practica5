a99ea5090fa988727201ef8862d06c993dde6b5d
Paulina Santillan <paulinasantillan@comunidad.unam.mx>
2025-03-06 21:24:06 -0600

    nuevos cambios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Box, Container, Link, Typography} from "@mui/material";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function Copyright() {
+    return (
+        <Typography>
+            { 'Copyright ' }
+            <Link href="https://github.com/paulinasant">
+                {' Git '}
+            </Link>
+            { new Date().getFullYear() }
+        </Typography>);
 }
 
-export default App;
+export default function MyApp() {
+    return (
+        <Container maxWidth="sm">
+            <Box sx={{my: 4}}>
+                <Typography variant="h4" component="h1" sx={{mb: 2}}>
+                    Mi primera App con React y Material UI
+                </Typography>
+                <Copyright/>
+
+            </Box>
+        </Container>
+    );
+}
+
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Practica 5</h1>
+                <selectAcceso />
+            </header>
+        </div>
+    );
+}
