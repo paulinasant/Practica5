58b7503162b395743f3d78b50bdfe824ace90fda
Paulina Santillan <paulinasantillan@comunidad.unam.mx>
2025-03-07 01:38:19 -0600

    nuevos cambios

--- src/App.js
+++ src/App.js
@@ -3,6 +3,36 @@ import React from 'react';
 import './App.css';
 import {Box, Container, Link, Typography} from "@mui/material";
 
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+function ButtonAppBar() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu"
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+
+                    </Typography>
+                    <Button color="inherit">Acceso</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
+
 
 function Copyright() {
     return (
@@ -19,8 +49,9 @@ export default function MyApp() {
     return (
         <Container maxWidth="sm">
             <Box sx={{my: 4}}>
+                <ButtonAppBar/>
                 <Typography variant="h4" component="h1" sx={{mb: 2}}>
-                    Mi primera App con React y Material UI
+                    Mi primera App con React y Material UI uwu
                 </Typography>
                 <Copyright/>
 
